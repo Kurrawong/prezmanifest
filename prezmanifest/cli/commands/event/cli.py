4eef3c1671c77b3acbf944fef057bc6e7bf3d25d
Edmond Chuc <edmond@kurrawong.ai>
2025-11-24 15:21:21 +1000

    test: add event sync test for azure service bus

--- prezmanifest/cli/commands/event/cli.py
+++ prezmanifest/cli/commands/event/cli.py
@@ -1,5 +1,6 @@
 import typer
 
+from prezmanifest.cli.commands.event.asb import app as asb_app
 from prezmanifest.cli.commands.event.rdf_delta import app as rdf_delta_app
 
 event_app = typer.Typer(name="event", help="Event-based Prez Manifests actions")
@@ -8,3 +9,4 @@ event_app.add_typer(
     sync_app, name="sync", help="Synchronize a Prez Manifest with an event system"
 )
 sync_app.add_typer(rdf_delta_app)
+sync_app.add_typer(asb_app)
